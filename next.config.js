997f1b514369e5e8bae04e61d39c9b9d2c443fd0
Eliott Berlemont <eliott.berlemont@gmail.com>
2026-05-07 23:11:09 -0700

    idk claude told me

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
-
-if (process.env.NODE_ENV === 'development') {
-  setupDevPlatform();
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
